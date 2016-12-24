48d2b2b2688c36f9e38b4178c99dd0ba5722243c
ansaries <arslan@ansaries.com>
2016-12-25 00:54:30 +0400

    Update package.js

--- package.js
+++ package.js
@@ -1,18 +1,16 @@
 Package.describe({
-  name: 'gwendall:accounts-helpers',
+  name: 'ansaries:accounts-helpers',
   summary: 'Useful helpers for Accounts',
-  version: '0.1.6',
-  git: 'https://github.com/gwendall/meteor-accounts-helpers'
+  version: '0.1.7',
+  git: 'https://github.com/ansaries/meteor-accounts-helpers'
 });
 
 var packages = [
   'check@1.0.6',
   'accounts-base@1.2.1',
   'accounts-ui@1.1.6',
-  'underscore@1.0.4',
-  'templating@1.1.4',
-  'service-configuration@1.0.5',
-  'gwendall:body-events@0.1.6',
+  'underscore@1.0.4',  
+  'service-configuration@1.0.5',  
   'aldeed:simple-schema@1.3.3'
 ];
 
